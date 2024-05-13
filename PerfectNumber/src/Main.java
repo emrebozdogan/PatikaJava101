3d051122f49098753680ac26dd9c0aa5b1668ba6
emrebozdogan <129841366+emrebozdogan@users.noreply.github.com>
2024-05-13 20:00:46 +0300

    Add files via upload

---
+++ PerfectNumber/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int total = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+        }
+        if (total == number){
+            System.out.println(number + " is a perfect number!");
+        }else{
+            System.out.println(number + " is not a perfect number!");
+        }
+    }
+}
